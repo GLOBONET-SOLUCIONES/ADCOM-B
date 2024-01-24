0ba714f2a0daefae086b765866e1512df7e29c1e
Oskar2524 <oscarquijano2524@gmail.com>
2024-01-23 21:11:53 -0500

    Fusion de codigo

---
+++ app/Http/Controllers/Administracion/ProveedoresEmpleados/ProveedorEmpreadoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Administracion\ProveedoresEmpleados;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProveedorEmpreadoController extends Controller
+{
+    //
+}
